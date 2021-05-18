8d431c36b0ebd1bcab5859b37a7c4dbc381e4b41
esteban1810 <estebansevillapg@gmail.com>
2021-05-17 20:52:16 -0500

    Funciones implementadas

--- app.js
+++ app.js
@@ -7,7 +7,12 @@
          )
 */
 
+const fs = require('fs');
+const path='./tareas.json';
+
 exports.leerTareas = function () {
+    const tareas = fs.readFileSync('./tareas.json');
+    return JSON.parse(tareas);
     /*
         En esta función debes leer y retornar las tareas registradas.
     */
@@ -17,6 +22,8 @@ exports.agregarTarea = function (tarea) {
     /*
         Registra y guarda una nueva tarea.
     */
+    const tareas = JSON.parse(fs.readFileSync('./tareas.json'));
+    fs.writeFileSync(path,JSON.stringify([...tareas,tarea]));
 }
 
 exports.filtrarTareasPorEstado = function (estado) {
@@ -24,4 +31,7 @@ exports.filtrarTareasPorEstado = function (estado) {
         En esta función debes de leer las tareas y retornar las que tengan el estado que se
         manda en el parametro.
     */
+    const tareas  = JSON.parse(fs.readFileSync('./tareas.json'));
+    return tareas.filter(e=>e.status===estado);
+    // return tareas;
 }
